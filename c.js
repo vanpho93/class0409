7245a759cfaed1cc48b2e20513286db8e09f5cd2
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-11 18:07:59 +0700

    functions

--- c.js
+++ c.js
@@ -1,4 +1,15 @@
+// phuong thuc
 function Person(ten, tuoi) {
     this.name = ten;
     this.age = tuoi;
+    this.sayHello = function() {
+        // console.log('Xin chao, toi la ' + this.name + ', ' + this.age + ' tuoi');
+        console.log(`Xin chao toi la ${this.name}, ${this.age} tuoi.`);
+    }
 }
+
+const person = new Person('Khoa', 20);
+const person2 = new Person('Teo', 10);
+
+person.sayHello();
+person2.sayHello();
