bbef48323ad69e30cf608d63393b9574c4e5e70e
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-11 18:51:59 +0700

    Line

--- f.js
+++ f.js
@@ -1,3 +1,29 @@
 // class Point 2 thuoc tinh x, y
 // class Line 2 thuoc tinh pointA, pointB
+// class Triangle, pointA, pointB, PointC, getPerimeter()
 // phuong thuc tinh ra do dai cua Line getLength()
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+const pA = new Point(0, 0);
+const pB = new Point(1, 0);
+
+class Line {
+    constructor(pointA, pointB) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+    }
+
+    getLength() {
+        const dx = this.pointA.x - this.pointB.x;
+        const dy = this.pointA.y - this.pointB.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+const l1 = new Line(pA, pB);
+console.log(l1.getLength());
